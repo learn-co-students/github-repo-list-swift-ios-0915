d5d68929dccc4ab9774540eb1bc972c3fe424695
Ryan Rosello <roselloryan@gmail.com>
2016-11-29 23:48:09 -0600

    passes all local tests no green light

--- github-repo-list-swift/GithubAPIClient.swift
+++ github-repo-list-swift/GithubAPIClient.swift
@@ -1,15 +1,47 @@
-//
-//  FISGithubAPIClient.swift
-//  github-repo-list-swift
-//
-//  Created by  susan lovaglio on 10/23/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
 
 import UIKit
 
 class GithubAPIClient {
     
+    enum madeUpErrors: Error {
+        case stupid
+        case moreThanStupid
+    }
+    
+    class func getRepositories(with completion: @escaping ([Dictionary <String, Any>]) -> Void) {
+        
+        guard let myURL = URL(string: "https://api.github.com/repositories?client_id=\(Constant.ClientId)&client_secret=\(Constant.ClientSecret)")
+            else {
+                print("\n\nURL INIT FAILED\n\n")
+                //throw madeUpErrors.moreThanStupid
+                return
+        }
+        let urlRequest = URLRequest(url: myURL)
+        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if error != nil {
+                print("ERROR ERROR ERROR: \(error?.localizedDescription)")
+            }
+            else {
+                if response != nil {
+                    if let httpResponse = response as? HTTPURLResponse {
+                        print("\nresponse code = \(httpResponse.statusCode)\n")
+                    }
+                }
+        
+                if data != nil {
+                    let responseArray = try? JSONSerialization.jsonObject(with: data!, options: []) as! [Dictionary <String, Any>]
+                
+                    //print("\nDATA: \(data)\n")
+                    //print("\nresponseDict: \(responseDict)")
+                    completion(responseArray!)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    //class func retrieveSpecific
 }
     
 
--- github-repo-list-swift/GithubRepository.swift
+++ github-repo-list-swift/GithubRepository.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class GithubRepository {
     
+    let fullName: String
+    let htmlURL: URL
+    let repositoryID: String
+
+    init(dictionary: Dictionary <String, Any>) {
+        self.fullName = dictionary["full_name"] as! String
+        let htmlString = dictionary["html_url"] as! String
+        self.htmlURL = URL(string: htmlString)!
+        let idInt = dictionary["id"] as! Int
+        self.repositoryID = String(idInt)
+    }
+/*
+    init(fullname: String, htmlURLstring: String, repositoryID: String) {
+        self.fullname = fullname
+        self.htmlURL = URL(string: htmlURLstring)!
+        self.repositoryID = repositoryID
+    }
+ */
 }
+
+/*
+ - `fullName` should be a stored property of type `String`. It will represent the full name of the repository object (which will be retrieved from the dictionary we get back from the API request).
+ - `htmlURL` should be a stored property of type `URL`. The dictionary we retrieve from the API request has a `String` value for the key `html_url`. This value returned back by this key is of type `String` (hint: It might need to be casted as a `String`). After doing so, you can then pass that `String` value to the initializer of `URL` to create an instance of `URL` that can be assigned as a value to the `htmlURL` stored property.
+ - `repositoryID` should be a stored property of type `String`. This will represent the ID of the repository (again, this is retrieved from the dictionary).
+ */
